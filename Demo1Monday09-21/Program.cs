3f4012f13bdf867d70596f9cde36a7598fc360f6
julle <julle@JOELSMEDION>
2026-09-21 09:44:24 +0200

    Add project files.

---
+++ Demo1Monday09-21/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Channels;
+
+namespace Demo1Monday09_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad heter du");
+            string namn = Console.ReadLine() ?? "";
+
+            bool resultat = LieDetector();
+            if (resultat)
+            {
+                Console.WriteLine("Du talar sanning");
+            }
+            else
+            {
+                Console.WriteLine("Du ljuger");
+            }
+        }
+        static bool LieDetector()
+        {
+            Random random = new Random();
+            int resultat = random.Next(0, 2);
+
+            return resultat == 1;
+        }
+    }
+}
